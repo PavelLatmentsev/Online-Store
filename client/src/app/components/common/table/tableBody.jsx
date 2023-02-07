4f30077f080b0b4bc14b7c94245b75882cb051a0
PavelLatmentsev <nightcharm@mail.ru>
2023-02-07 18:06:36 +0300

    fix key map

--- client/src/app/components/common/table/tableBody.jsx
+++ client/src/app/components/common/table/tableBody.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import TableItem from "./tableItem";
 import PropTypes from "prop-types";
+import { nanoid } from "nanoid";
 
 const TableBody = ({ products, isBaseProdacts }) => {
     return (
         <tbody>
             {products.map((product, index) => (
-                <TableItem product={product} key={product._id} index={index} isBaseProdacts={isBaseProdacts} />
+                <TableItem product={product} key={isBaseProdacts ? product._id : nanoid() } index={index} isBaseProdacts={isBaseProdacts} />
             ))}
 
         </tbody>
