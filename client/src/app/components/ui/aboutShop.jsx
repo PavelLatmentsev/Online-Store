9d7318f412a66e6aeeaf44812629938ac839b5f2
PavelLatmentsev <nightcharm@mail.ru>
2023-02-11 18:35:14 +0300

    add slider on main page

--- client/src/app/components/ui/aboutShop.jsx
+++ client/src/app/components/ui/aboutShop.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import styles from "./aboutShop.module.scss";
 import NavButton from "../common/uniButton";
 import aboutShop from "../../assets/background/aboutShop.png";
+import { useHistory } from "react-router-dom";
 const AboutShop = () => {
+    const history = useHistory();
+    const heandleClick = () => {
+        history.push("/about");
+      };
     return (<div className={styles.wrapper}>
         <div className={styles.container}>
             <div className={styles.aboutShop}>
@@ -13,7 +18,7 @@ const AboutShop = () => {
                     <p className={styles.aboutShop_titleName}>Тату магазин Mr. Driskell</p>
                     <p className={styles.aboutShop_titleAbout}>Приветствуем вас в Tattoo Mall — в нашем тату магазине собираются энтузиасты индустрии, профессиональные мастера и новички, которые только делают первые шаги в тату искусстве. Мы знаем, насколько важно грамотно и точно подобрать инструменты для продуктивных тату сеансов, и помогаем быстро найти то, что поможет их сделать именно такими.
                         В нашем ассортименте не просто тату оборудование, это буквально целая команда из грамотных и действительно эффективных помощников на вашем рабочем столе. Пройдя этап долгих разработок и бесчисленных тестов под пристальным взглядом отечественных машиностроителей, космецевтов и брендов с мировым именем, эти товары нарабатывали опыт и каждый день становились лучше, чтобы показать, на что они способны, и помочь раскрыть ваш потенциал.</p>
-                    <div className={styles.aboutShop_titleBtn}><NavButton fill="#EEEEEE" color="#BB8C5F" title="О компании" /></div>
+                    <div className={styles.aboutShop_titleBtn}><NavButton fill="#EEEEEE" color="#BB8C5F" title="О компании" onChange={heandleClick} /></div>
                 </div>
             </div>
         </div>
--- client/src/app/components/ui/sliderList.jsx
+++ client/src/app/components/ui/sliderList.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import styles from "./sliderList.module.scss";
 import NavButton from "../common/uniButton";
 import { useHistory } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay } from "swiper";
 const SliderList = () => {
   const history = useHistory();
   const heandleClick = () => {
@@ -11,28 +16,60 @@ const SliderList = () => {
     <section>
       <div className={styles.sliderList_box}>
         <div className={styles.container}>
-          <div className={styles.sliderList}>
-            <div className={styles.sliderList_block}>
-              <h1 className={styles.sliderList_block_title}>
-                Лучшие товары <br /> в мире татуировок
-              </h1>
-              <p className={styles.sliderList_block_description}>
-                Оборудование и расходники <br /> для самых ярких и качественных
-                работ
-              </p>
-              <div className={styles.sliderList_blockBtn}>
-
-                <span>
-                  <NavButton
-                    fill="#524336"
-                    color="#FAF6F2"
-                    title="Смотреть каталог"
-                    onChange={heandleClick}
-                  />
-                </span>
+          <Swiper
+            spaceBetween={1}
+            centeredSlides={false}
+            // autoplay={{
+            //   delay: 2500,
+            //   disableOnInteraction: false
+            // }}
+            navigation={false}
+            modules={[Autoplay]}
+            className="mySwiper"
+          >
+            {" "}
+            <SwiperSlide>
+              {" "}
+              <div className={styles.sliderList}>
+                <div className={styles.sliderList_block}>
+                  <h1 className={styles.sliderList_block_title}>
+                    Лучшие товары <br /> в мире татуировок
+                  </h1>
+                  <p className={styles.sliderList_block_description}>
+                    Оборудование и расходники <br /> для самых ярких и
+                    качественных работ
+                  </p>
+                  <div className={styles.sliderList_blockBtn}>
+                    <span>
+                      <NavButton
+                        fill="#524336"
+                        color="#FAF6F2"
+                        title="Смотреть каталог"
+                        onChange={heandleClick}
+                      />
+                    </span>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </SwiperSlide>
+            <SwiperSlide><div className={styles.sliderList_itemFirst}></div></SwiperSlide>
+            <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemSecond}></div></SwiperSlide>
+          <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemThird}></div></SwiperSlide>
+          <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemFour}></div></SwiperSlide>
+          <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemFive}></div></SwiperSlide>
+          <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemSix}></div></SwiperSlide>
+          <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemSeven}></div></SwiperSlide>
+          <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemEight}></div></SwiperSlide>
+          <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemNine}></div></SwiperSlide>
+          <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemTen}></div></SwiperSlide>
+          <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemEleven}></div></SwiperSlide>
+          <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemTwelve}></div></SwiperSlide>
+          <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemThirteen}></div></SwiperSlide>
+          <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemFourTeen}></div></SwiperSlide>
+           <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemFifTeen}></div></SwiperSlide>
+          {/* <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemSixTeen}></div></SwiperSlide>
+          <SwiperSlide><div className={styles.sliderList_itemFirst + " " + styles.itemSevenTeen}></div></SwiperSlide> */}
+          </Swiper>
         </div>
       </div>
     </section>
